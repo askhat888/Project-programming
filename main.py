2fb0c16ee71b3aa8361d3dd39b08fed90ebf0e36
askhat888 <amandykovaskat888@mail.ru>
2022-12-14 11:05:53 +0500

    Добавление графики

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tanks")
 fontUI = pygame.font.Font(None, 30)
 fontRes = pygame.font.Font(None, 24)
+imgBrick = pygame.image.load('block_brick.png')
+imgTanks = [
+    pygame.image.load('tank1.png'),
+    pygame.image.load('tank2.png'),
+    pygame.image.load('tank3.png'),
+    pygame.image.load('tank4.png'),
+    pygame.image.load('tank5.png'),
+    pygame.image.load('tank6.png'),
+    pygame.image.load('tank7.png'),
+    pygame.image.load('tank8.png'),
+]
+imgBangs = [
+    pygame.image.load('bang1.png'),
+    pygame.image.load('bang2.png'),
+    pygame.image.load('bang3.png'),
+]
 play = True
 while play:
 
