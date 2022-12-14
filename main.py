1832cebbd153d878c0448c0025a594b1e0792953
askhat888 <amandykovaskat888@mail.ru>
2022-12-14 11:12:08 +0500

    Создание карты

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from random import randint
 pygame.init()
 WIDTH, HEIGHT = 800, 600
 FPS = 60
+TILE = 32
 clock = pygame.time.Clock()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tanks")
@@ -26,13 +27,46 @@ imgBangs = [
     pygame.image.load('bang2.png'),
     pygame.image.load('bang3.png'),
 ]
+class Block:
+    def __init__(self, px, py, size):
+        objects.append(self)
+        self.type = 'block'
+
+        self.rect = pygame.Rect(px, py, size, size)
+        self.hp = 1
+
+    def update(self):
+        pass
+
+    def draw(self):
+        window.blit(imgBrick, self.rect)
+
+    def damage(self, value):
+        self.hp -= value
+        if self.hp <= 0:
+            objects.remove(self)
+objects = []
+for _ in range(50):
+    while True:
+        x = randint(0, WIDTH // TILE - 1) * TILE
+        y = randint(1, HEIGHT // TILE - 1) * TILE
+        rect = pygame.Rect(x, y, TILE, TILE)
+        fined = False
+        for obj in objects:
+            if rect.colliderect(obj.rect): fined = True
+
+        if not fined: break
+
+    Block(x, y, TILE)
 play = True
 while play:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
-
+    for obj in objects: obj.update()
+    window.fill((0, 0, 0))
+    for obj in objects: obj.draw()
     pygame.display.update()
     clock.tick(FPS)
 
