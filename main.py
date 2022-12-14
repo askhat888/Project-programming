b465fbf785b637e53084bd41ab0f5273bb17ad19
askhat888 <amandykovaskat888@mail.ru>
2022-12-14 11:04:37 +0500

    Создание окна для игры

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import pygame
+from PIL import Image
+from random import randint
 
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+clock = pygame.time.Clock()
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tanks")
+fontUI = pygame.font.Font(None, 30)
+fontRes = pygame.font.Font(None, 24)
+play = True
+while play:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            play = False
+
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
